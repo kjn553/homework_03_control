6f16cd58c3b4bb4f46b31e4e5b640af97536ab44
Mike E <mikhailnev03@gmail.com>
2024-05-12 09:31:44 +0700

    task 2

--- src/main/kotlin/Task1_agoToText.kt
+++ src/main/kotlin/Task1_agoToText.kt
@@ -1,5 +1,33 @@
 package ru.netology
 
 fun main() {
-    println("Hello World!")
+    println(agoToText(90001))
 }
+
+val personName  = "Вася"
+fun agoToText(seconds: Int): String {
+    return when {
+        seconds < 60 -> "$personName был(а) в сети только что"
+        seconds < 60 * 60 -> "$personName был(а) в сети ${minuteForm(seconds / 60)} назад"
+        seconds < 24 * 60 * 60 -> "$personName был(а) в сети ${seconds / 3600} ${hourForm(seconds / 3600)} назад"
+        seconds < 2 * 24 * 60 * 60 -> "$personName был(а) в сети вчера"
+        seconds < 3 * 24 * 60 * 60 -> "$personName был(а) в сети позавчера"
+        else -> "$personName был(а) в сети давно"
+    }
+}
+fun minuteForm(minutes: Int): String {
+    return when (minutes) {
+        1, 21, 31, 41, 51 -> "минуту"
+        2, 3, 4, 22, 23, 24, 32, 33, 34, 42, 43, 44, 52, 53, 54 -> "минуты"
+        else -> "минут"
+    }
+}
+fun hourForm(hours: Int): String {
+    return when (hours) {
+        1, 21 -> "час"
+        2, 3, 4, 22, 23, 24 -> "часа"
+        else -> "часов"
+    }
+}
+
+
---
+++ src/main/kotlin/Task2_improvedCommission.kt
@@ -0,0 +1,50 @@
+package ru.netology
+
+fun main() {
+    val cardType1 = "MASTERCARD"
+    val cardType2 = "VISA"
+    val cardType3 = "MIR"
+    val cardType4 = "MAESTRO"
+    val previousTransfersThisMonth = 20_000.0
+    val transferAmount = 10_000.0
+    println(calculateCommission(cardType1, previousTransfersThisMonth, transferAmount))
+    println(calculateCommission(cardType2, previousTransfersThisMonth, transferAmount))
+    println(calculateCommission(cardType3, previousTransfersThisMonth, transferAmount))
+    println(calculateCommission(cardType4, previousTransfersThisMonth, transferAmount))
+}
+
+fun calculateCommission(cardType: String, previousTransfersThisMonth: Double, transferAmount: Double): String {
+    val dailyLimit = 150_000.0
+    val monthlyLimit = 600_000.0
+    var result = ""
+    when {
+        transferAmount > dailyLimit || previousTransfersThisMonth + transferAmount > monthlyLimit -> {
+            result = "${cardType}: Превышен лимит переводов. Транзакция заблокирована."
+        }
+         cardType == "MASTERCARD" -> {
+            val monthlyLimitNoCommission = 75000.0
+            if (previousTransfersThisMonth + transferAmount <= monthlyLimitNoCommission) {
+                result = "${cardType}: 0 рублей."
+            } else {
+                val exceededAmount = (previousTransfersThisMonth + transferAmount) - monthlyLimitNoCommission
+                result = "${cardType}: ${exceededAmount * 0.006 + 20} рублей."
+            }
+        }
+        cardType == "VISA" -> {
+            val minCommission = 35.0
+            val commission = transferAmount * 0.0075
+            if (commission < minCommission) {
+                result = "$minCommission рублей."
+            } else {
+                result = "${cardType}: $commission рублей."
+            }
+        }
+        cardType == "MIR" -> {
+            result = "${cardType}: 0 рублей."
+        }
+        else -> {
+            result = "Ошибка: неверный тип карты."
+        }
+    }
+    return result
+}
